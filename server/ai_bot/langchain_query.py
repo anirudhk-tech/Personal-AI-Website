1c2b1f9c5be0fe833925e2bc964f9911cf87f172
Anirudh Kuppili <anirudhkuppili@gmail.com>
2024-09-15 13:01:37 -0500

    Batching implemented for efficient memory usage. Hitting 512MB roof limit for Render.

--- server/ai_bot/langchain_query.py
+++ server/ai_bot/langchain_query.py
@@ -8,9 +8,11 @@ from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from ai_bot.dataset import *
 
-transformer_modelPath = "sentence-transformers/paraphrase-TinyBERT-L6-v2" # Defining information about transformer model for embedding
+transformer_modelPath = "sentence-transformers/all-MiniLM-l6-v2" # Defining information about transformer model for embedding
 model_kwargs={'device': 'cpu'}
 encode_kwargs = {'normalize_embeddings': True}
+db = None # Initializing index of data
+batch_size = 20 # Process 20 documents at a time
 
 embeddings = HuggingFaceEmbeddings( # Creating embedding object for embedding
     model_name=transformer_modelPath,
@@ -18,11 +20,18 @@ embeddings = HuggingFaceEmbeddings( # Creating embedding object for embedding
     encode_kwargs=encode_kwargs,
 )
 
-db = FAISS.from_documents(processed_data, embeddings) # Initializing index of data
-finder = db.as_retriever(search_kwargs={"k": 5}) # Initializing retriever
+def batching (data, batch_size): # Function to batch documents for efficient memory usage
+    for x in range(0, len(data), batch_size):
+        yield data[x: x + batch_size]
 
 def search_faiss (query): # Search function that fetches relevant context
+    finder = db.as_retriever(search_kwargs={"k": 5}) # Initializing retriever
     searchResult = finder.get_relevant_documents(query)
-
     return [searchResult[x].metadata["response"] for x in range(5)] # Returning top 5 results
+
+for batch in batching(processed_data, batch_size):
+    if not db:
+        db = FAISS.from_documents(batch, embeddings) # Create index if not created yet
+    else:
+        db.add_documents(batch) # Add documents once index is created
   
